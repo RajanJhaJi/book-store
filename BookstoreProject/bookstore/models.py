f6a436d2f9e65acf63d3159b8dc127163b0fa397
RajanJhaJi <rajan29jha@gmail.com>
2023-10-23 08:48:49 +0530

    adding extra validation

--- BookstoreProject/bookstore/models.py
+++ BookstoreProject/bookstore/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
@@ -6,7 +8,12 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     image = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
+    publication_year = models.IntegerField(
+        validators=[
+            MinValueValidator(1000, message="Publication year must be at least 1000."),
+            MaxValueValidator(2023, message="Publication year cannot exceed 2023."),
+        ],
+    )
     isbn = models.CharField(max_length=13)
     description = models.TextField()
 
